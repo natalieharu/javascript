a6e0828ec3288190c7568f494cfaaba41ffa8143
natalieharu <natalicooper@gmail.com>
2020-10-12 17:57:08 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+const category = 'toys';
+console.log(`https://someul.com/${category}/5`);
+
+const user = 'Ihar';
+alert(`Hello, ${user}`);
+console.log(8 + 'kyk');
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+const isChecked = true,
+    isClose = false;
+console.log(isChecked || isClose);
